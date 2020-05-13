badd49f5b15fc6b9146d484b41c6313df72f6a64
jghori <59810056+jghori@users.noreply.github.com>
2020-05-13 18:38:05 -0400

    Updates

---
+++ precache-manifest.5082cd034a942609ed295ef8da6c7f26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f24592806ff8538a73a06ec346370f5d",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "ddf1d71ea9c730e57e60",
+    "url": "/shopping-cart/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9a4bb820c26817e101c8",
+    "url": "/shopping-cart/static/css/main.57fe5805.chunk.css"
+  },
+  {
+    "revision": "ddf1d71ea9c730e57e60",
+    "url": "/shopping-cart/static/js/2.8b49488e.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/shopping-cart/static/js/2.8b49488e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a4bb820c26817e101c8",
+    "url": "/shopping-cart/static/js/main.6f2daae0.chunk.js"
+  },
+  {
+    "revision": "f78ee7b71997a9b5e1df",
+    "url": "/shopping-cart/static/js/runtime-main.2044215a.js"
+  }
+]);
--- src/actions/getAction.js
+++
@@ -1,9 +0,0 @@
-import { GET_NUMBERS_CART } from "./types";
-
-export const getNumber = () => {
-  return (dispatch) => {
-    dispatch({
-      type: GET_NUMBERS_CART,
-    });
-  };
-};
